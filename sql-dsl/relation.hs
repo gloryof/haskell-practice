8016697550aad47b320c50fe836c05a17a358c08
Junki Yamada <geek.junkey@gmail.com>
2016-01-21 22:28:38 +0900

    とりあえず動くものができたのでコミット

--- sql-dsl/relation.hs
+++ sql-dsl/relation.hs
@@ -1,14 +1,36 @@
-data Relation = Relation String deriving Show
 data Selection = Selection [String] deriving Show
 data Projection = Projection [String] deriving Show
+data Relation = Relation String Selection Projection  deriving Show
 
-rel :: String -> Relation
-rel = Relation
+class Semantics exp where
+  toSql:: exp -> String
 
-toSql :: Relation -> String
-toSql (Relation x) = "SELECT * FROM " ++ x
+instance Semantics Relation where
+  toSql(Relation nm rel prj) = "SELECT " ++ toSql rel ++ " FROM " ++ nm ++ toSql prj
+
+instance Semantics Selection where
+  toSql(Selection []) = "*"
+  toSql(Selection (x:[])) = x
+  toSql(Selection (x:xs)) = x ++ ", " ++ toSql (Selection xs)
+
+instance Semantics Projection where
+  toSql(Projection []) = ""
+  toSql(Projection (x:xs)) = " WHERE " ++  x ++ foldl (\acc x -> acc ++ " AND " ++ x) "" xs
+
+rel :: String -> Selection -> Projection -> Relation
+rel x slc prj = Relation x slc prj
+
+extract :: [String] -> Selection
+extract xs = (Selection xs)
+
+project :: [String] -> Projection
+project xs = (Projection xs)
 
 {-
-extract :: Relation -> Selection -> Relation
-proj :: Relation -> Projection -> Relation
+toSql $ rel "hoge" (extract ["fuga = 1", "bar = 2"]) (project ["fuga, bar"])
+なら動くけど、
+rel "hoge" . extract ["fuga = 1", "bar = 2"] . project ["fuga, bar"]
+みたいにしたい。
+
+rel "hoge" >>= extract ["fuga = 1", "bar = 2"] >= project ["fuga, bar"]じゃないとダメ？
 -}
