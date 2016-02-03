f4acce27cbec51ec67b8aa4cbd30d0bcb3884fc3
Junki Yamada <geek.junkey@gmail.com>
2016-02-03 22:00:33 +0900

    モナドっぽくしてみた

--- sql-dsl/relation.hs
+++ sql-dsl/relation.hs
@@ -1,36 +1,40 @@
+data Name = Name (Maybe String) deriving Show
 data Selection = Selection [String] deriving Show
 data Projection = Projection [String] deriving Show
-data Relation = Relation String Selection Projection  deriving Show
+data Relation = Relation Name Selection Projection  deriving Show
 
-class Semantics exp where
-  toSql:: exp -> String
+relation :: String ->  Relation -> Maybe Relation
+relation nm (Relation _ slc prj) = Just (Relation (Name (Just nm)) slc prj)
 
-instance Semantics Relation where
-  toSql(Relation nm rel prj) = "SELECT " ++ toSql rel ++ " FROM " ++ nm ++ toSql prj
+extract :: [String] -> Relation -> Maybe Relation
+extract slc (Relation nm _ prj) = Just (Relation nm (Selection slc) prj)
 
-instance Semantics Selection where
+projection :: [String] ->  Relation -> Maybe Relation
+projection prj (Relation nm slc _) = Just (Relation nm slc (Projection prj))
+
+class Sql a where
+  toSql :: a -> String
+
+instance Sql Relation where
+  toSql(Relation nm rel prj) = "SELECT " ++ toSql rel ++ toSql nm ++ toSql prj
+
+instance Sql Name where
+  toSql (Name Nothing) = ""
+  toSql (Name (Just x)) = " FROM " ++ x
+
+instance Sql Selection where
   toSql(Selection []) = "*"
   toSql(Selection (x:[])) = x
   toSql(Selection (x:xs)) = x ++ ", " ++ toSql (Selection xs)
 
-instance Semantics Projection where
+instance Sql Projection where
   toSql(Projection []) = ""
   toSql(Projection (x:xs)) = " WHERE " ++  x ++ foldl (\acc x -> acc ++ " AND " ++ x) "" xs
 
-rel :: String -> Selection -> Projection -> Relation
-rel x slc prj = Relation x slc prj
-
-extract :: [String] -> Selection
-extract xs = (Selection xs)
-
-project :: [String] -> Projection
-project xs = (Projection xs)
+rel :: String -> Maybe Relation
+rel x  = Just (Relation (Name (Just x)) (Selection []) (Projection []))
 
 {-
-toSql $ rel "hoge" (extract ["fuga = 1", "bar = 2"]) (project ["fuga, bar"])
-なら動くけど、
-rel "hoge" . extract ["fuga = 1", "bar = 2"] . project ["fuga, bar"]
-みたいにしたい。
-
-rel "hoge" >>= extract ["fuga = 1", "bar = 2"] >= project ["fuga, bar"]じゃないとダメ？
+rel "hoge" >>= extract ["fuga = 1", "bar = 2"] >= projection ["fuga, bar"]
+今度は toSqlできない・・・
 -}
