92f8db87ec115a4ce78aab31538bb9fe271e5381
Junki Yamada <geek.junkey@gmail.com>
2016-05-10 21:24:24 +0900

    現状で一旦セーブポイントを作りたいのでコミット

--- html-dsl/html.hs
+++ html-dsl/html.hs
@@ -3,6 +3,7 @@
 
 import Data.Maybe (fromMaybe)
 import Data.Map as Map (Map,insert,empty,lookup,fromList)
+import Data.Map.Lazy as ML (foldlWithKey)
 
 escape :: String -> String
 escape x = x
@@ -13,9 +14,24 @@ type TagName = String
 
 data Element = Element TagName (Map.Map String [String]) [Element]
              | EmpElement String deriving Show
+
 class ElemFunc x where
   aid :: x -> String -> x
   cls :: x -> String -> x
+
+{- toHtml "" $ dv (\n -> [(cls (spn "test") "tes-class"), (aid (cls (cls (spn "test2") "test-class") "test2-class") "test-id"), (spn "test3")]) -}
+toHtml :: String -> Element -> String
+toHtml ind (EmpElement ct) = ind ++ ct
+toHtml ind (Element tn as es) = let atr = if length as == 0 then "" else " " ++ (outputAttr as)
+                                    op = "<" ++ tn ++ atr ++  ">"
+                                    ct = foldl (\acc x -> acc ++ (toHtml "  " x) ) "" es
+                                    ed = "</" ++ tn ++ ">"
+                                in ind ++ op ++ ct ++ ed
+
+outputAttr :: Map String [String] -> String
+outputAttr am = let exp vals = "\"" ++ (unwords vals) ++ "\""
+                    join k v = k ++ "=" ++ (exp v)
+                in ML.foldlWithKey (\acc k v -> acc ++ (join k v)) "" am
 
 instance ElemFunc Element where
   aid (Element tn as es) v = (Element tn (insert "id" [v] as) es)
@@ -23,6 +39,9 @@ instance ElemFunc Element where
                                  new = insert "class" (bas ++ [v]) as
                              in (Element tn new es)
 
+{- html (\n -> [(spn "test"),(spn "test2"),(spn "test3")]) -}
+html :: (() -> [Element]) -> Element
+html fn = Element "html" Map.empty(fn())
 
 {- dv (\n -> [(spn "test"), (spn "test2") , (spn "test3")]) -}
 dv :: (() -> [Element]) -> Element
